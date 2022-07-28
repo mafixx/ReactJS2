2ed1b6e604bf25bae3cae369cbab5c88c09842e6
Pedro Barbieri <pedro.romaguera@hotmail.com>
2022-07-28 18:34:50 -0300

    index and icon change

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import styles from "./SearchBar.module.css";
 
-export default function SearchBar(){
+export default function SearchBar() {
+    const [filterText, setFilterText] = useState("");
+
     return (
         <form className={styles.form}>
-            <input type="text" placeholder="Search..." />
+            {/* Componente controlado */}
+            <input 
+                type="text" 
+                placeholder="Search..."
+                onChange={e => setFilterText(e.target.value)}
+                value={filterText} 
+            />
             <label>
                 <input type="checkbox" />Only show products in stock
             </label>
